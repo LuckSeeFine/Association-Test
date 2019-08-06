9eb23df5a1fd355fd887b8d9e67e00bdb5fc9a3b
g-yoshimi <g-yoshimi@ruby-dev.jp>
2019-08-06 14:12:30 +0900

    お気に入り登録&解除追加

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_08_05_073634) do
+ActiveRecord::Schema.define(version: 2019_08_06_004854) do
+
+  create_table "favorites", force: :cascade do |t|
+    t.integer "user_id"
+    t.integer "tweet_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "tweets", force: :cascade do |t|
     t.text "body"
